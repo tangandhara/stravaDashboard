86d43c00d15172ee5e4f70ed6fc3bb51fc72ed39
Tan Gandhara <63885085+tangandhara@users.noreply.github.com>
2024-05-31 23:13:04 +0100

    Update

---
+++ Strava.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(leaflet)
+library(rStrava)
+library(sf)
+library(httr)
+
+app_name <- "My API" # chosen by user
+app_client_id  <- 102029 # an integer, assigned by Strava
+app_secret <- "67db4a62becf14e519ee00852585cd8b8c7407b0" # an alphanumeric secret, assigned by Strava
+
+stoken <- httr::config(token = strava_oauth(app_name, app_client_id, app_secret, cache = TRUE, app_scope="activity:read_all"))
+
+stoken <- httr::config(token = readRDS('.httr-oauth')[[1]])
+
+strava_token <- httr::config(token = strava_oauth(app_name,
+                                                  app_client_id,
+                                                  app_secret,
+                                                  app_scope = "activity:read_all"))
+my_acts <- get_activity_list(strava_token) 
+id = 10695060340
+strava_data <- get_activity_streams(my_acts,
+                                    strava_token,
+                                    id = id)
+strava_data
+write.csv(strava_data, "strava_data.csv", row.names = F)
+library(tidyverse)
+strava_data %>% 
+  as_tibble() %>% 
+  select(-id)
+
+library(sf)
+library(ggmap)
+library(osmdataa)
+library(rcartocolor)
+library(gganimate)
+
+
+bb <- c(-0.4465567, -0.3302629, 50.8017364, 50.8632589)
+
+
+bg_map <- get_map(location = bb,
+                  source = "stadia",
+                  maptype = "stamen_terrain", 
+                  color = "bw",
+                  scale = 4)
+
+register_google(key = "AIzaSyDPYVyRPbqAUYqxeqlODV7TaKiONCQKjXA")
+
+
+ggmap(bg_map)
+
